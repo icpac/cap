9e11de896bf124f57a40281dff3fd3d4fc2450a3
iCPAC <cj.icpac@gmail.com>
2020-04-07 08:55:20 -0500

    Clientes

---
+++ cap.Clientes/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cap.Clientes
+{
+    public class Class1
+    {
+    }
+}
