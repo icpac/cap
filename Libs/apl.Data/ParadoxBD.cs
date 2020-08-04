3a3000a42d39c982961aad1813389681c9a8ba7a
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-08-04 10:29:25 -0500

    Add files via upload
    Para leer datos de Paradox

---
+++ Libs/apl.Data/ParadoxBD.cs
@@ -0,0 +1,128 @@
+#region Copyright (c) 2000-2012 cjlc
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     cjlc control administrativo personal                          }
+{                                                                   }
+{     Copyrigth (c) 2000-2012 cjlc                                  }
+{     Todos los derechos reservados                                 }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace apl.Data
+{
+    using System.Data;
+    using System.Data.Odbc;
+    using System.Collections.Specialized;
+
+    public class ParadoxBD : GenericBD
+    {
+        #region + override Get Blob parameter
+        protected override IDataParameter GetBlobParameter(IDbConnection connection, IDataParameter p)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+        #endregion
+
+        #region + override obten coneccion con cadena de coneccion
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cadenaConexion"></param>
+        /// <returns></returns>
+        public override IDbConnection obtenConexion(string cadenaConexion)
+        {
+            return new OdbcConnection(cadenaConexion);
+        }
+        #endregion
+
+        #region # Create data adapter
+        protected override IDbDataAdapter CreateDataAdapter()
+        {
+            return new OdbcDataAdapter();
+        }
+        #endregion
+
+        #region + override obten parametro
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override IDataParameter obtenParametro()
+        {
+            return new OdbcParameter();
+        }
+        #endregion
+
+        #region + override Borra tabla
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nameTable"></param>
+        public override void BorraTabla(string nameTable)
+        {
+            /*
+            if (ExisteTabla(nameTable))
+            {
+                string sqlTabla;
+                IDataParameter[] parames = null;
+
+                sqlTabla = "DROP TABLE ";
+                sqlTabla += nameTable;
+
+                ejecutaSentencia(sqlTabla, parames);
+            }*/
+        }
+        #endregion
+
+        #region + Crea tabla
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nameTable"></param>
+        /// <param name="campos"></param>
+        /// <param name="tipos"></param>
+        /// <param name="tamanos"></param>
+        /// <param name="keys"></param>
+        /// <param name="indexs"></param>
+        /// <param name="uniques"></param>
+        /// <returns></returns>
+        public override bool CreaTabla(string nameTable, StringCollection campos, StringCollection tipos,
+            StringCollection tamanos, StringCollection keys, StringCollection indexs, StringCollection uniques)
+        {
+            /*
+            bool ok = false;
+
+            if (!ExisteTabla(nameTable))
+            {
+                string sqlTabla;
+                IDataParameter[] parames = null;
+
+                sqlTabla = SentenciaTabla(nameTable, campos, tipos, tamanos, keys, indexs, uniques);
+                ejecutaSentencia(sqlTabla, parames);
+
+                if (indexs.Count > 0)
+                    foreach (string campo in indexs)
+                    {
+                        sqlTabla = "CREATE INDEX Indice" + campo;
+                        sqlTabla += " ON " + nameTable + " ( " + campo + " )";
+                        ejecutaSentencia(sqlTabla, parames);
+                    }
+
+                ok = true;
+            }
+            else
+            {
+                VerificaTabla(nameTable, campos, tipos, tamanos);
+            }
+            return ok;*/
+            return false;
+        }
+        #endregion
+    }
+}
