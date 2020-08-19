cb811e0edbed6ef19442f9dd6590813847db6ebb
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-08-19 16:53:26 -0500

    Add files via upload

---
+++ Escul/Escul.Module/Controllers/VCAsignatura.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using Escul.Module.BusinessObjects;
+using DevExpress.Spreadsheet;
+
+namespace Escul.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class VCAsignatura : ViewController
+    {
+        public VCAsignatura()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+            if (View is DetailView && View.ObjectSpace != null)
+            {
+                Parametros prm = View.ObjectSpace.FindObject<Parametros>(null);
+
+                if (prm != null)
+                    FileSystemData.FileSystemDataModule.FileSystemStoreLocation = prm.RutaArchvs;
+            }
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
