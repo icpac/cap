aba2e328efaf8bab9933bbbf8105c347f400a51c
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-08-27 09:46:34 -0500

    Add files via upload

---
+++ TCap/TCap.Module/Controllers/Proyectos/VCActividad.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TCap.Module.Controllers.Proyectos
+{
+    partial class VCActividad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.popupWindowShowActionActvddCmpltd = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.simpleActionActvddEjccn = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.simpleActionActvddSspndr = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.popupWindowShowActionActvddSspndr = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            // 
+            // popupWindowShowActionActvddCmpltd
+            // 
+            this.popupWindowShowActionActvddCmpltd.AcceptButtonCaption = "Listo";
+            this.popupWindowShowActionActvddCmpltd.CancelButtonCaption = null;
+            this.popupWindowShowActionActvddCmpltd.Caption = "Completada";
+            this.popupWindowShowActionActvddCmpltd.Category = "Edit";
+            this.popupWindowShowActionActvddCmpltd.ConfirmationMessage = null;
+            this.popupWindowShowActionActvddCmpltd.Id = "2ca59123-c5e6-4a78-8f74-cc0c27c02cd7";
+            this.popupWindowShowActionActvddCmpltd.ToolTip = null;
+            this.popupWindowShowActionActvddCmpltd.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionActvddCmpltd_CustomizePopupWindowParams);
+            this.popupWindowShowActionActvddCmpltd.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionActvddCmpltd_Execute);
+            // 
+            // simpleActionActvddEjccn
+            // 
+            this.simpleActionActvddEjccn.Caption = "En Ejecución";
+            this.simpleActionActvddEjccn.Category = "Edit";
+            this.simpleActionActvddEjccn.ConfirmationMessage = "Está seguro de Iniciar la Actividad?";
+            this.simpleActionActvddEjccn.Id = "4828753c-4027-460a-9d7c-b19895b60ab3";
+            this.simpleActionActvddEjccn.ToolTip = null;
+            this.simpleActionActvddEjccn.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionActvddEjccn_Execute);
+            // 
+            // simpleActionActvddSspndr
+            // 
+            this.simpleActionActvddSspndr.Caption = "Suspeder Actividad";
+            this.simpleActionActvddSspndr.ConfirmationMessage = "Está seguro de Suspender la Actividad?";
+            this.simpleActionActvddSspndr.Id = "660afd78-9c16-49d8-89e6-bca137270718";
+            this.simpleActionActvddSspndr.ToolTip = null;
+            this.simpleActionActvddSspndr.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionActvddSspndr_Execute);
+            // 
+            // popupWindowShowActionActvddSspndr
+            // 
+            this.popupWindowShowActionActvddSspndr.AcceptButtonCaption = "Suspender";
+            this.popupWindowShowActionActvddSspndr.CancelButtonCaption = null;
+            this.popupWindowShowActionActvddSspndr.Caption = "Suspender Actividad";
+            this.popupWindowShowActionActvddSspndr.Category = "Edit";
+            this.popupWindowShowActionActvddSspndr.ConfirmationMessage = "Está seguro de Suspender?";
+            this.popupWindowShowActionActvddSspndr.Id = "461707b0-b917-4f12-988f-d3647692dd79";
+            this.popupWindowShowActionActvddSspndr.ToolTip = null;
+            this.popupWindowShowActionActvddSspndr.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionActvddSspndr_CustomizePopupWindowParams);
+            this.popupWindowShowActionActvddSspndr.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionActvddSspndr_Execute);
+            // 
+            // VCActividad
+            // 
+            this.Actions.Add(this.popupWindowShowActionActvddCmpltd);
+            this.Actions.Add(this.simpleActionActvddEjccn);
+            this.Actions.Add(this.simpleActionActvddSspndr);
+            this.Actions.Add(this.popupWindowShowActionActvddSspndr);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionActvddCmpltd;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionActvddEjccn;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionActvddSspndr;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionActvddSspndr;
+    }
+}
---
+++ TCap/TCap.Module/Controllers/Proyectos/VCActividad.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using TCap.Module.BusinessObjects.Proyectos;
+
+namespace TCap.Module.Controllers.Proyectos
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class VCActividad : ViewController
+    {
+        public VCActividad()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+
+            TargetObjectType = typeof(Actividad);
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+
+            popupWindowShowActionActvddCmpltd.TargetObjectType = (typeof(Actividad));
+            popupWindowShowActionActvddCmpltd.TargetObjectsCriteria = "Stts = 'Ejecución'";
+
+            simpleActionActvddEjccn.TargetObjectType = typeof(Actividad);
+            simpleActionActvddEjccn.TargetObjectsCriteria = "Stts = 'Espera'";
+
+            simpleActionActvddSspndr.TargetObjectType = typeof(Actividad);
+            simpleActionActvddSspndr.TargetObjectsCriteria = "Stts = 'Espera' || Stts = 'Ejecución'";
+
+            popupWindowShowActionActvddSspndr.TargetObjectType = typeof(Actividad);
+            popupWindowShowActionActvddSspndr.TargetObjectsCriteria = "Stts = 'Espera' || Stts = 'Ejecución'";
+            popupWindowShowActionActvddSspndr.ConfirmationMessage = null;
+
+            simpleActionActvddSspndr.Active.SetItemValue("Visible", false);
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void popupWindowShowActionActvddCmpltd_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            ActividadCompletada newObj;
+
+            newObj = objectSpace.FindObject<ActividadCompletada>(null);
+            if (newObj == null)
+                newObj = objectSpace.CreateObject<ActividadCompletada>();
+            newObj.FchTrmncn = DateTime.Now;
+            e.View = Application.CreateDetailView(objectSpace, "ActividadCompletada_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionActvddCmpltd_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            ActividadCompletada obj = e.PopupWindowViewCurrentObject as ActividadCompletada;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Actividad act = View.CurrentObject as Actividad;
+
+                act.FchFn = obj.FchTrmncn;
+                act.Stts = obj.Stts;
+                act.AlarmTime = null;
+
+                Incidencia inc = os.CreateObject<Incidencia>();
+
+                inc.Dscrpcn = obj.Dscrpcn;
+                inc.Hrs = obj.Hrs;
+                inc.Fch = obj.FchTrmncn;
+
+                act.Incidencias.Add(inc);
+                os.CommitChanges();
+            }
+        }
+
+        private void simpleActionActvddEjccn_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            if (View != null)
+            {
+                Actividad obj = View.CurrentObject as Actividad;
+
+                obj.FchInc = DateTime.Today;
+                obj.Stts = TASKSTATUS.Ejecución;
+
+                View.ObjectSpace.CommitChanges();
+            }
+        }
+
+        private void simpleActionActvddSspndr_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            if (View != null && View.CurrentObject != null)
+            {
+                Actividad act = View.CurrentObject as Actividad;
+
+                act.Stts = TASKSTATUS.Suspendida;
+
+                View.ObjectSpace.CommitChanges();
+            }
+        }
+
+        private void popupWindowShowActionActvddSspndr_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            ActividadSuspendida newObj = objectSpace.CreateObject<ActividadSuspendida>();
+            e.View = Application.CreateDetailView(objectSpace, "ActividadSuspendida_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionActvddSspndr_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            ActividadSuspendida obj = e.PopupWindowViewCurrentObject as ActividadSuspendida;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Actividad act = View.CurrentObject as Actividad;
+
+                act.FchFn = obj.Fch;
+                act.Stts = obj.Stts;
+
+                Incidencia inc = os.CreateObject<Incidencia>();
+
+                inc.Dscrpcn = obj.Nts;
+                // inc.Hrs = obj.Hrs;
+                inc.Fch = obj.Fch;
+
+                act.Incidencias.Add(inc);
+                os.CommitChanges();
+            }
+        }
+    }
+}
