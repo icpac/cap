e5277fc7187ccf7cff15d234d11faafdd8f1e0bb
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-08-27 04:03:30 -0500

    Add files via upload

---
+++ Libs/apl.Log/EPeriodicidad.cs
@@ -0,0 +1,26 @@
+﻿#region 2014-
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     tlacaelel.icpac                                               }
+{     Cap control administrativo personal                           }
+{                                                                   }
+{     2014-                                                         }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+
+namespace apl.Log
+{
+    public enum EPeriodicidad
+    {
+        Diaria = 1,
+        Semanal = 2,
+        Quincenal = 3,
+        Mensual = 4,
+        Bimestral = 5,
+        Invalido = -1
+    }
+}
