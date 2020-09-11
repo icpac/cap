9189e81d1a0caf9a0581b52881cd08a0f3df9dcd
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-09-11 04:02:31 -0500

    Add files via upload

---
+++ Gral/Cap.Bancos/BusinessObjects/EConceptoTipo.cs
@@ -0,0 +1,31 @@
+﻿#region Copyright (c) 2014-2020 cjlc
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     cjlc Cap control administrativo personal                      }
+{                                                                   }
+{     Copyrigth (c) 2014-2020 cjlc                                  }
+{     Todos los derechos reservados                                 }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+using DevExpress.Persistent.Base;
+
+namespace Cap.Bancos.BusinessObjects
+{
+    //#region + Concepto tipo enums
+    public enum EConceptoTipo
+    {
+        /*
+        Salida = 1,
+        Entrada = 2,*/
+        [ImageName("Label-Red")]
+        Cargo = 3,
+        [ImageName("Label-Green")]
+        Abono = 4,
+        Ninguno = 5
+    }
+    //#endregion
+}
