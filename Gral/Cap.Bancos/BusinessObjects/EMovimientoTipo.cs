070c7efb2e3e4586ee4fd209442c78e962639dab
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-09-14 04:42:00 -0500

    Add files via upload

---
+++ Gral/Cap.Bancos/BusinessObjects/EMovimientoTipo.cs
@@ -0,0 +1,27 @@
+﻿#region 2014-2020 tit
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     Cap control administrativo personal                           }
+{                                                                   }
+{     2014-2020 tit                                                 }
+{     tlacaelel.icpac@gmail.com                                     }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+using DevExpress.ExpressApp.DC;
+
+namespace Cap.Bancos.BusinessObjects
+{
+    public enum EMovimientoTipo
+    {
+        Normal = 1,
+        Cheque = 2,
+        Gasto = 3,
+        [XafDisplayName("Transf. Electrónica F.")]
+        TEF = 4,
+        Ninguno = 5
+    }
+}
