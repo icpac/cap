f3f26cc23c1f37c0f9403853be50c1bc34194dd0
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-09-16 05:49:58 -0500

    Add files via upload

---
+++ Gral/Cap.Bancos/BusinessObjects/MontoGTipo.cs
@@ -0,0 +1,23 @@
+﻿#region 2014-2020 tit
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     Cap control administrativo personal                           }
+{                                                                   }
+{     2014-2020 tit                                                 }
+{     tlacaelel.icpac@gmail.com                                     }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+
+namespace Cap.Bancos.BusinessObjects
+{
+    public enum MontoGTipo
+    {
+        Cantidad = 1,
+        Porcentaje = 2,
+        Ninguno = 3
+    }
+}
