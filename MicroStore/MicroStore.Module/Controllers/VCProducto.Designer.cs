77b495318a91c4dbe4c1c9b195c20602a7ed8196
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-09-20 17:08:17 -0500

    Add files via upload

---
+++ MicroStore/MicroStore.Module/Controllers/VCProducto.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace MicroStore.Module.Controllers
+{
+    partial class VCProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.popupWindowShowActionActPrecs = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            // 
+            // popupWindowShowActionActPrecs
+            // 
+            this.popupWindowShowActionActPrecs.AcceptButtonCaption = "Actualizar";
+            this.popupWindowShowActionActPrecs.CancelButtonCaption = "Cancelar";
+            this.popupWindowShowActionActPrecs.Caption = "Actualizar Precios";
+            this.popupWindowShowActionActPrecs.ConfirmationMessage = null;
+            this.popupWindowShowActionActPrecs.Id = "4a601862-4627-497d-89c2-c5e3d478e4b8";
+            this.popupWindowShowActionActPrecs.ToolTip = null;
+            this.popupWindowShowActionActPrecs.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionActPrecs_CustomizePopupWindowParams);
+            this.popupWindowShowActionActPrecs.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionActPrecs_Execute);
+            // 
+            // VCProducto
+            // 
+            this.Actions.Add(this.popupWindowShowActionActPrecs);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionActPrecs;
+    }
+}
---
+++ MicroStore/MicroStore.Module/Controllers/VCProducto.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cap.Inventarios.BusinessObjects;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using MicroStore.Module.Utilerias;
+
+namespace MicroStore.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class VCProducto : ViewController
+    {
+        public VCProducto()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+
+            TargetObjectType = typeof(Producto);
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+
+            if (View is DetailView)
+            {
+                foreach (PropertyEditor editor in ((DetailView)View).GetItems<PropertyEditor>())
+                {
+                    // disable controls in DetailView
+                    if (editor.PropertyName == "Existencia"
+                        || editor.PropertyName == "CostoUltimo")
+                    {
+                        editor.AllowEdit.SetItemValue("AllowEdit", false);
+                    }
+                    else
+                    {
+                        editor.AllowEdit.RemoveItem("AllowEdit");
+                    }
+                }
+
+                PropertyEditor pt = ((DetailView)View).GetItems<PropertyEditor>().First(p => p.PropertyName == "Tipo");
+
+                pt.AllowEdit.SetItemValue("AllowEdit", false);
+            }
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void popupWindowShowActionActPrecs_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            ProductoFiltroPrecio fil = objectSpace.CreateObject<ProductoFiltroPrecio>();
+
+            e.View = Application.CreateDetailView(objectSpace, "ProductoFiltroPrecio_DetailView", true, fil);
+        }
+
+        private void popupWindowShowActionActPrecs_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            ProductoFiltroPrecio dlgFil = e.PopupWindowViewCurrentObject as ProductoFiltroPrecio;
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+
+            if (dlgFil != null)
+            {
+                if (dlgFil.InputFile != null)
+                {
+                    Negocio.ActualizaPreciosFile(dlgFil, objectSpace);
+                }
+                else
+                { 
+                    Negocio.ActualizaPrecios(dlgFil, objectSpace);
+                }
+            }
+        }
+    }
+}
