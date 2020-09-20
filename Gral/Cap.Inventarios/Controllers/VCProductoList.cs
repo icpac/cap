5c39e8fb2db33d1b04eabd7f62a9d6f6dd9e36f5
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-09-20 17:06:33 -0500

    Add files via upload

---
+++ Gral/Cap.Inventarios/Controllers/VCProductoList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using Cap.Inventarios.BusinessObjects;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+
+namespace Cap.Inventarios.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class VCProductoList : ViewController
+    {
+        public VCProductoList()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+            TargetObjectType = typeof(Producto);
+            TargetViewType = ViewType.ListView;
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
