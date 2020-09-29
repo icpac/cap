63a2ef86f5165a19e53716f57d66476d73e3b353
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-09-29 09:45:06 -0500

    Add files via upload

---
+++ MicroStore/MicroStore.Module/Controllers/VCInventarioFisicoImportar.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace MicroStore.Module.Controllers
+{
+    partial class VCInventarioFisicoImportar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.popupWindowShowActionImprtrFl = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            // 
+            // popupWindowShowActionImprtrFl
+            // 
+            this.popupWindowShowActionImprtrFl.AcceptButtonCaption = "Importar";
+            this.popupWindowShowActionImprtrFl.CancelButtonCaption = null;
+            this.popupWindowShowActionImprtrFl.Caption = "Importar Inventario Físico";
+            this.popupWindowShowActionImprtrFl.ConfirmationMessage = null;
+            this.popupWindowShowActionImprtrFl.Id = "783f04e9-7fc6-4229-9e5a-50486e03d182";
+            this.popupWindowShowActionImprtrFl.ToolTip = null;
+            this.popupWindowShowActionImprtrFl.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionImprtrFl_CustomizePopupWindowParams);
+            this.popupWindowShowActionImprtrFl.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionImprtrFl_Execute);
+            // 
+            // VCInventarioFisicoImportar
+            // 
+            this.Actions.Add(this.popupWindowShowActionImprtrFl);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionImprtrFl;
+    }
+}
---
+++ MicroStore/MicroStore.Module/Controllers/VCInventarioFisicoImportar.cs
@@ -0,0 +1,64 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using Cap.Inventarios.BusinessObjects;
+using MicroStore.Module.Utilerias;
+
+namespace MicroStore.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class VCInventarioFisicoImportar : ViewController
+    {
+        public VCInventarioFisicoImportar()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+            TargetObjectType = typeof(InventarioFisico);
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+
+        private void popupWindowShowActionImprtrFl_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            InvFisImporta fil = objectSpace.FindObject<InvFisImporta>(null);
+
+            if (fil == null)
+                fil = objectSpace.CreateObject<InvFisImporta>();
+
+            e.View = Application.CreateDetailView(objectSpace, "InvFisImporta_DetailView", true, fil);
+        }
+
+        /*
+        private void popupWindowShowActionImprtrFl_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            e.View = Application.CreateDetailView(this.Application.CreateObjectSpace(), newFileInputParameter());
+            ((DetailView)e.View).ViewEditMode = ViewEditMode.Edit;
+        }
+
+        private FileInputParameter newFileInputParameter()
+        {
+            return View.ObjectSpace.CreateObject<FileInputParameter>();
+        }*/
+
+        private void popupWindowShowActionImprtrFl_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            InvFisImporta parameter = (InvFisImporta)e.PopupWindowViewCurrentObject;
+
+            Negocio.ImportaInvFis(parameter, Application.CreateObjectSpace());
+        }
+    }
+}
