2ef6ad47758068dac8d44d9482bb44caa6c6775b
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-10-01 13:56:49 -0500

    Add files via upload

--- Gral/Cap.Generales/DatabaseUpdate/Updater.cs
+++ Gral/Cap.Generales/DatabaseUpdate/Updater.cs
@@ -121,14 +121,6 @@ namespace Cap.Generales.DatabaseUpdate
             mones.Add("EUR|Euro|||1|0");
             mones.Add("USD|Dólar estadounidense/US Dollar|Dollar/Dollars|USD|1|0");
             mones.Add("MXN|Peso Mexicano|PESO/PESOS|M.N.|0|1");
-            /*
-            TI Ago el SAT nos da un catálogo de Monedas !  Tal vez hay que dejarlo porque el
-            otro es muy grande
-            mones.Add("GTQ|Quetzal|QUETZAL/QUETZALES|Q|2|1");
-            mones.Add("CLP|Peso Chileno|PESO/PESOS|CLP|2|1");
-            mones.Add("COP|Peso Colombiano|PESO/PESOS|COP|2|1");
-            mones.Add("ARS|Peso Argentino|PESO/PESOS|ARS|2|1");
-            mones.Add("PEN|Nuevo Sol|NUEVO SOL/NUEVOS SOLES|S/.|2|1");*/
 
             foreach (string ob in mones)
             {
@@ -151,27 +143,6 @@ namespace Cap.Generales.DatabaseUpdate
                 }
             }
 
-            /*
-            Empresa emp = ObjectSpace.FindObject<Empresa>(null); 
-
-            if (emp == null)
-            {
-                emp = ObjectSpace.CreateObject<Empresa>();
-                emp.Compania.Nombre = "EMPRESA INVALIDA";
-                emp.Compania.Rfc = "AAA010101AAA";
-                emp.Compania.Curp = "CURPEMPRESA";
-                emp.Regimen = "Regimen de la empresa";
-                emp.Compania.Direccion.Calle = "Calle Empresa";
-                emp.Compania.Direccion.Numero = "54";
-                emp.Compania.Direccion.Colonia = "Tepelmeme Villa de Morelos";
-                emp.Compania.Direccion.Municipio = "Tepelmeme Villa de Morelos";
-                emp.Compania.Direccion.CP = "69310";
-                emp.Compania.Direccion.Estado = "Uaxaca";
-                emp.Clave = "icpac";
-                emp.Usuario = "pruebasWS";
-                emp.Passw = "pruebasWS";
-            }*/
-
             /*TIT Lo hacemos en el Factory
              Abr 19, si no lo creo al salir del detalle me pregunta 
              si deseo salvar, pero como usuario pienso: no he hecho nada, 
@@ -193,60 +164,6 @@ namespace Cap.Generales.DatabaseUpdate
             }
 
             ObjectSpace.CommitChanges();
-            /*
-            Ventas vta = ObjectSpace.FindObject<Ventas>(CriteriaOperator.Parse("Clave = 'icpac'"));
-            if (vta == null)
-            {
-                vta = ObjectSpace.CreateObject<Ventas>();
-                vta.Save();
-            }*/
-
-            /*
-            bool altaS = false;
-            Certificado cer = ObjectSpace.FindObject<Certificado>(CriteriaOperator.Parse("Clave = 'icpac'"));
-            if (cer == null)
-            {
-                cer = ObjectSpace.CreateObject<Certificado>();
-                cer.Clave = "icpac";
-                cer.PasswCertif = "12345678a";
-                cer.Save();
-
-                altaS = true;
-            }*/
-
-            /*
-            if (altaS)
-            {
-                Producto srvc = ObjectSpace.FindObject<Producto>(CriteriaOperator.Parse("Clave = 'SRVC'"));
-                if (srvc == null)
-                {
-                    srvc = ObjectSpace.CreateObject<Producto>();
-                    srvc.Clave = "SRVC";
-                    srvc.Descripcion = "SERVICIO CON IMPUESTO";
-                    srvc.Tipo = EProductoTipo.Servicio;
-                    srvc.CapEnFact = true;
-                    srvc.USalida =
-                    srvc.UProducto = ObjectSpace.FindObject<Unidad>(CriteriaOperator.Parse("Clave = 'NA'"));
-                    srvc.Esquema = ObjectSpace.FindObject<EsquemaImpuesto>(CriteriaOperator.Parse("Clave = '1'"));
-
-                    srvc.Save();
-                }
-
-                srvc = ObjectSpace.FindObject<Producto>(CriteriaOperator.Parse("Clave = 'SRVC2'"));
-                if (srvc == null)
-                {
-                    srvc = ObjectSpace.CreateObject<Producto>();
-                    srvc.Clave = "SRVC2";
-                    srvc.Descripcion = "SERVICIO SIN IMPUESTO";
-                    srvc.Tipo = EProductoTipo.Servicio;
-                    srvc.CapEnFact = true;
-                    srvc.USalida =
-                    srvc.UProducto = ObjectSpace.FindObject<Unidad>(CriteriaOperator.Parse("Clave = 'NA'"));
-                    srvc.Esquema = ObjectSpace.FindObject<EsquemaImpuesto>(CriteriaOperator.Parse("Clave = '2'"));
-
-                    srvc.Save();
-                }
-            }*/
         }
     }
 }
