ade7099e08581174db4f30d922098451b8580a0d
Tlacaelel Icpac <cj.icpac@gmail.com>
2021-03-09 15:11:27 -0600

    Add files via upload

---
+++ SSRV/SSRV.Module/Controllers/VCIncidencia.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SSRV.Module.Controllers
+{
+    partial class VCIncidencia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleActionReincidencia = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.popupWindowShowActionIncdncRspnsbl = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.popupWindowShowActionIncdncStart = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.popupWindowShowActionIncdncEspr = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.popupWindowShowActionIncdncTrmnd = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.popupWindowShowActionIncdncCncld = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.popupWindowShowActionAgrgrBtcr = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.simpleActionCnclr = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // simpleActionReincidencia
+            // 
+            this.simpleActionReincidencia.Caption = "Reabrir Reporte";
+            this.simpleActionReincidencia.ConfirmationMessage = "Está seguro de abrirlo?";
+            this.simpleActionReincidencia.Id = "efc512d2-7513-4450-baef-cfb0eda907ad";
+            this.simpleActionReincidencia.SelectionDependencyType = DevExpress.ExpressApp.Actions.SelectionDependencyType.RequireSingleObject;
+            this.simpleActionReincidencia.ToolTip = null;
+            this.simpleActionReincidencia.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionReincidencia_Execute);
+            // 
+            // popupWindowShowActionIncdncRspnsbl
+            // 
+            this.popupWindowShowActionIncdncRspnsbl.AcceptButtonCaption = null;
+            this.popupWindowShowActionIncdncRspnsbl.CancelButtonCaption = null;
+            this.popupWindowShowActionIncdncRspnsbl.Caption = "Asigna Responsable";
+            this.popupWindowShowActionIncdncRspnsbl.ConfirmationMessage = null;
+            this.popupWindowShowActionIncdncRspnsbl.Id = "d455489a-7a6c-44f6-9740-50c9b948689a";
+            this.popupWindowShowActionIncdncRspnsbl.ToolTip = null;
+            this.popupWindowShowActionIncdncRspnsbl.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionIncdncHrr_CustomizePopupWindowParams);
+            this.popupWindowShowActionIncdncRspnsbl.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionIncdncHrr_Execute);
+            // 
+            // popupWindowShowActionIncdncStart
+            // 
+            this.popupWindowShowActionIncdncStart.AcceptButtonCaption = null;
+            this.popupWindowShowActionIncdncStart.CancelButtonCaption = null;
+            this.popupWindowShowActionIncdncStart.Caption = "Atención en Curso";
+            this.popupWindowShowActionIncdncStart.ConfirmationMessage = null;
+            this.popupWindowShowActionIncdncStart.Id = "eebeda78-f073-4c25-ba6a-dc8625a3e548";
+            this.popupWindowShowActionIncdncStart.ToolTip = null;
+            this.popupWindowShowActionIncdncStart.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionIncdncStart_CustomizePopupWindowParams);
+            this.popupWindowShowActionIncdncStart.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionIncdncStart_Execute);
+            // 
+            // popupWindowShowActionIncdncEspr
+            // 
+            this.popupWindowShowActionIncdncEspr.AcceptButtonCaption = null;
+            this.popupWindowShowActionIncdncEspr.CancelButtonCaption = null;
+            this.popupWindowShowActionIncdncEspr.Caption = "Sin Solución";
+            this.popupWindowShowActionIncdncEspr.ConfirmationMessage = null;
+            this.popupWindowShowActionIncdncEspr.Id = "e6e6ee60-b413-4a85-8fae-ac0bf667c6e9";
+            this.popupWindowShowActionIncdncEspr.ToolTip = null;
+            this.popupWindowShowActionIncdncEspr.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionIncdncEspr_CustomizePopupWindowParams);
+            this.popupWindowShowActionIncdncEspr.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionIncdncEspr_Execute);
+            // 
+            // popupWindowShowActionIncdncTrmnd
+            // 
+            this.popupWindowShowActionIncdncTrmnd.AcceptButtonCaption = null;
+            this.popupWindowShowActionIncdncTrmnd.CancelButtonCaption = null;
+            this.popupWindowShowActionIncdncTrmnd.Caption = "Incidencia Resuelta";
+            this.popupWindowShowActionIncdncTrmnd.ConfirmationMessage = null;
+            this.popupWindowShowActionIncdncTrmnd.Id = "2e5f71ec-6439-46f9-b1c2-fe3128314834";
+            this.popupWindowShowActionIncdncTrmnd.ToolTip = null;
+            this.popupWindowShowActionIncdncTrmnd.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionIncdncTrmnd_CustomizePopupWindowParams);
+            this.popupWindowShowActionIncdncTrmnd.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionIncdncTrmnd_Execute);
+            // 
+            // popupWindowShowActionIncdncCncld
+            // 
+            this.popupWindowShowActionIncdncCncld.AcceptButtonCaption = null;
+            this.popupWindowShowActionIncdncCncld.CancelButtonCaption = null;
+            this.popupWindowShowActionIncdncCncld.Caption = "Cancelar";
+            this.popupWindowShowActionIncdncCncld.ConfirmationMessage = null;
+            this.popupWindowShowActionIncdncCncld.Id = "5d2a0661-d175-402e-991e-a57673d4b9d9";
+            this.popupWindowShowActionIncdncCncld.ToolTip = null;
+            this.popupWindowShowActionIncdncCncld.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionIncdncCncld_CustomizePopupWindowParams);
+            this.popupWindowShowActionIncdncCncld.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionIncdncCncld_Execute);
+            // 
+            // popupWindowShowActionAgrgrBtcr
+            // 
+            this.popupWindowShowActionAgrgrBtcr.AcceptButtonCaption = null;
+            this.popupWindowShowActionAgrgrBtcr.CancelButtonCaption = null;
+            this.popupWindowShowActionAgrgrBtcr.Caption = "Agregar Bitacora";
+            this.popupWindowShowActionAgrgrBtcr.ConfirmationMessage = null;
+            this.popupWindowShowActionAgrgrBtcr.Id = "69561462-8a90-4bac-8f72-898569a0a4f0";
+            this.popupWindowShowActionAgrgrBtcr.ToolTip = null;
+            this.popupWindowShowActionAgrgrBtcr.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionAgrgrBtcr_CustomizePopupWindowParams);
+            this.popupWindowShowActionAgrgrBtcr.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionAgrgrBtcr_Execute);
+            // 
+            // simpleActionCnclr
+            // 
+            this.simpleActionCnclr.Caption = "Cancelar";
+            this.simpleActionCnclr.ConfirmationMessage = "Está seguro de CANCELAR ?";
+            this.simpleActionCnclr.Id = "a3dbb6e8-be23-4527-9567-a1a5072d297d";
+            this.simpleActionCnclr.ToolTip = null;
+            this.simpleActionCnclr.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionCnclr_Execute);
+            // 
+            // VCIncidencia
+            // 
+            this.Actions.Add(this.simpleActionReincidencia);
+            this.Actions.Add(this.popupWindowShowActionIncdncRspnsbl);
+            this.Actions.Add(this.popupWindowShowActionIncdncStart);
+            this.Actions.Add(this.popupWindowShowActionIncdncEspr);
+            this.Actions.Add(this.popupWindowShowActionIncdncTrmnd);
+            this.Actions.Add(this.popupWindowShowActionIncdncCncld);
+            this.Actions.Add(this.popupWindowShowActionAgrgrBtcr);
+            this.Actions.Add(this.simpleActionCnclr);
+            this.ViewControlsCreated += new System.EventHandler(this.VCIncidencia_ViewControlsCreated);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionReincidencia;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionIncdncRspnsbl;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionIncdncStart;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionIncdncEspr;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionIncdncTrmnd;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionIncdncCncld;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionAgrgrBtcr;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionCnclr;
+    }
+}
--- SSRV/SSRV.Module/Controllers/VCIncidencia.cs
+++ SSRV/SSRV.Module/Controllers/VCIncidencia.cs
@@ -1,1 +1,382 @@
+﻿using System;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using SSRV.Module.BusinessObjects.Servicio;
+using SSRV.Module.Utilerias;
+using SSRV.Module.BusinessObjects;
+using SSRV.Module.BusinessObjects.Admin;
 
+namespace SSRV.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class VCIncidencia : ViewController
+    {
+        public VCIncidencia()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+            TargetObjectType = typeof(Incidencia);
+
+            simpleActionReincidencia.TargetObjectType = typeof(Incidencia);
+            simpleActionReincidencia.TargetObjectsCriteria = "Status = 'OPERATIVO'";
+
+            popupWindowShowActionIncdncRspnsbl.TargetObjectType = typeof(Incidencia);
+            popupWindowShowActionIncdncRspnsbl.TargetObjectsCriteria = "Status == 'ASIGNACION' || Status == 'PENDIENTE'";
+            popupWindowShowActionIncdncRspnsbl.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+
+            popupWindowShowActionIncdncStart.TargetObjectType = typeof(Incidencia);
+            popupWindowShowActionIncdncStart.TargetObjectsCriteria = "Status == 'ESPERA'";
+            popupWindowShowActionIncdncStart.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+
+            popupWindowShowActionIncdncEspr.TargetObjectType = typeof(Incidencia);
+            popupWindowShowActionIncdncEspr.TargetObjectsCriteria = "Status == 'SITIO'";
+            popupWindowShowActionIncdncEspr.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+
+            popupWindowShowActionIncdncTrmnd.TargetObjectType = typeof(Incidencia);
+            popupWindowShowActionIncdncTrmnd.TargetObjectsCriteria = "Status == 'SITIO'";
+            popupWindowShowActionIncdncTrmnd.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+
+            popupWindowShowActionIncdncCncld.TargetObjectType = typeof(Incidencia);
+            popupWindowShowActionIncdncCncld.TargetObjectsCriteria = "Status != 'CANCELADO' && Status != 'OPERATIVO'";
+            popupWindowShowActionIncdncCncld.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+
+            popupWindowShowActionAgrgrBtcr.TargetObjectType = typeof(Incidencia);
+            popupWindowShowActionAgrgrBtcr.TargetObjectsCriteria = "Status != 'OPERATIVO' && Status != 'CANCELADO'";
+
+            simpleActionCnclr.TargetObjectType = typeof(Incidencia);
+            simpleActionCnclr.TargetObjectsCriteria = "Status != 'CANCELADO'";
+            simpleActionCnclr.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+
+            if (View != null && View is DetailView)
+            {
+                if (View.ObjectSpace != null)
+                    View.ObjectSpace.Committing += ObjectSpace_Committing;
+            }
+
+            // No sé por el momento si será así
+            // Ago 19
+            simpleActionReincidencia.Active.SetItemValue("Visible", false);
+            popupWindowShowActionIncdncRspnsbl.Active.SetItemValue("Visible", false);
+            popupWindowShowActionIncdncStart.Active.SetItemValue("Visible", false);
+            popupWindowShowActionIncdncTrmnd.Active.SetItemValue("Visible", false);
+            popupWindowShowActionIncdncEspr.Active.SetItemValue("Visible", false);
+            popupWindowShowActionIncdncCncld.Active.SetItemValue("Visible", false);
+            popupWindowShowActionAgrgrBtcr.Active.SetItemValue("Visible", false);
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        void ObjectSpace_Committing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (View != null)
+            {
+                Incidencia obj = View.CurrentObject as Incidencia;
+
+                if (obj != null)
+                {
+                    if (View.ObjectSpace.IsNewObject(obj))
+                    {
+                        NegocioSrvc.GrabaNewServicio(obj);
+                        e.Cancel = false;
+                    }
+                    else
+                    {
+                        NegocioSrvc.GrabaServicio(obj);
+                        e.Cancel = false;
+                    }
+                }
+            }
+        }
+
+        private void simpleActionReincidencia_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            Incidencia srv = View.CurrentObject as Incidencia;
+
+            // Create IObjectSpace or use the existing one, e.g. View.ObjectSpace, if it is suitable for your scenario.
+            IObjectSpace os = Application.CreateObjectSpace();
+
+            //Find an existing object.
+            //Contact obj = os.FindObject<Contact>(CriteriaOperator.Parse("FirstName=?", "My Contact"));
+            //Or create a new object.
+            Incidencia obj = os.CreateObject<Incidencia>();
+
+            if (srv != null)
+            {
+                obj.Fll = srv.Fll;
+                obj.Antrr = obj.Session.FindObject<Incidencia>(new BinaryOperator("Oid", srv.Oid));
+            }
+
+            //Save the changes if necessary.
+            //os.CommitChanges();
+
+            //Specify the IsRoot parameter if necessary.
+            DetailView dv = Application.CreateDetailView(os, obj);
+            dv.ViewEditMode = DevExpress.ExpressApp.Editors.ViewEditMode.Edit;
+            e.ShowViewParameters.CreatedView = dv;
+
+            //Specify various display settings if necessary
+            //e.ShowViewParameters.TargetWindow = TargetWindow.NewModalWindow;
+            //e.ShowViewParameters.Context = TemplateContext.PopupWindow;
+            e.ShowViewParameters.CreateAllControllers = true;
+            //You can pass custom Controllers for intercommunication or to provide a standard functionality).
+            //DialogController dc = Application.CreateController<DialogController>();
+            //e.ShowViewParameters.Controllers.Add(dc);
+        }
+
+        private void VCIncidencia_ViewControlsCreated(object sender, EventArgs e)
+        {
+            if (View != null)
+            {
+                Incidencia obj = View.CurrentObject as Incidencia;
+
+                if (View.ObjectSpace != null && View.ObjectSpace.IsNewObject(obj))
+                    NegocioSrvc.IniciaSrvc(obj);
+            }
+        }
+
+        private void popupWindowShowActionIncdncHrr_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            ResponsableAtencion newObj;
+
+            newObj = objectSpace.FindObject<ResponsableAtencion>(null);
+            if (newObj != null)
+                objectSpace.Delete(newObj);
+            /*
+            if (newObj == null)*/
+                newObj = objectSpace.CreateObject<ResponsableAtencion>();
+            e.View = Application.CreateDetailView(objectSpace, "ResponsableAtencion_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionIncdncHrr_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            ResponsableAtencion ha = e.PopupWindowViewCurrentObject as ResponsableAtencion;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Incidencia obj = View.CurrentObject as Incidencia;
+
+                obj.FchCt = ha.FchCt;
+                obj.TmCt = ha.TmCt;
+                obj.Rspnsbl = os.FindObject<Empleado>(new BinaryOperator("Oid", ha.Rspnsbl.Oid));
+                obj.Status = StsSrvc.ESPERA;
+
+                os.CommitChanges();
+            }
+        }
+
+        private void popupWindowShowActionIncdncStart_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            InicioAtencion newObj;
+
+            newObj = objectSpace.FindObject<InicioAtencion>(null);
+            if (newObj != null)
+                objectSpace.Delete(newObj);
+
+            /*
+            if (newObj == null)*/
+                newObj = objectSpace.CreateObject<InicioAtencion>();
+            /*
+            else
+            {
+                newObj.Fch = DateTime.Today;
+                newObj.Tm = DateTime.Now;
+            }*/
+            e.View = Application.CreateDetailView(objectSpace, "InicioAtencion_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionIncdncStart_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            InicioAtencion ia = e.PopupWindowViewCurrentObject as InicioAtencion;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Incidencia obj = View.CurrentObject as Incidencia;
+
+                obj.Status = StsSrvc.SITIO;
+
+                Bitacora btcr = os.CreateObject<Bitacora>();
+
+                btcr.FchIni = ia.Fch;
+                btcr.TimeIni = ia.Tm;
+                btcr.Observ = ia.Nts;
+                btcr.Stts = EBITCORATP.Inicio;
+
+                obj.Partidas.Add(btcr);
+                os.CommitChanges();
+            }
+        }
+
+        private void popupWindowShowActionIncdncEspr_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            PendienteAtencion newObj;
+
+            newObj = objectSpace.FindObject<PendienteAtencion>(null);
+            if (newObj != null)
+                objectSpace.Delete(newObj);
+
+            newObj = objectSpace.CreateObject<PendienteAtencion>();
+            e.View = Application.CreateDetailView(objectSpace, "PendienteAtencion_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionIncdncEspr_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            PendienteAtencion ia = e.PopupWindowViewCurrentObject as PendienteAtencion;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Incidencia obj = View.CurrentObject as Incidencia;
+
+                obj.Status = StsSrvc.PENDIENTE;
+
+                Bitacora btcr = os.CreateObject<Bitacora>();
+
+                btcr.FchIni = ia.Fch;
+                btcr.TimeIni = ia.Tm;
+                btcr.Observ = ia.Nts;
+                btcr.Stts = EBITCORATP.Proceso;
+
+                obj.Partidas.Add(btcr);
+                os.CommitChanges();
+            }
+        }
+
+        private void popupWindowShowActionIncdncTrmnd_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            SolucionAtencion newObj;
+
+            newObj = objectSpace.FindObject<SolucionAtencion>(null);
+            if (newObj != null)
+                objectSpace.Delete(newObj);
+
+            newObj = objectSpace.CreateObject<SolucionAtencion>();
+            e.View = Application.CreateDetailView(objectSpace, "SolucionAtencion_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionIncdncTrmnd_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            SolucionAtencion ia = e.PopupWindowViewCurrentObject as SolucionAtencion;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Incidencia obj = View.CurrentObject as Incidencia;
+
+                obj.Status = StsSrvc.OPERATIVO;
+
+                Bitacora btcr = os.CreateObject<Bitacora>();
+
+                btcr.FchFin = ia.Fch;
+                btcr.TimeFin = ia.Tm;
+                btcr.Dscrpcn = ia.Dscrpcn;
+                btcr.Atnd = ia.Atnd;
+                btcr.Observ = ia.Nts;
+                btcr.Stts = EBITCORATP.Terminacion;
+
+                obj.Partidas.Add(btcr);
+                os.CommitChanges();
+            }
+        }
+
+        private void popupWindowShowActionIncdncCncld_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            CancelaAtencion newObj;
+
+            newObj = objectSpace.FindObject<CancelaAtencion>(null);
+            if (newObj != null)
+                objectSpace.Delete(newObj);
+
+            newObj = objectSpace.CreateObject<CancelaAtencion>();
+            e.View = Application.CreateDetailView(objectSpace, "CancelaAtencion_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionIncdncCncld_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            CancelaAtencion ca = e.PopupWindowViewCurrentObject as CancelaAtencion;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Incidencia obj = View.CurrentObject as Incidencia;
+
+                obj.Status = StsSrvc.CANCELADO;
+
+                Bitacora btcr = os.CreateObject<Bitacora>();
+
+                btcr.FchFin = ca.Fch;
+                btcr.Observ = ca.Nts;
+                btcr.Stts = EBITCORATP.Cancelacion;
+
+                obj.Partidas.Add(btcr);
+                os.CommitChanges();
+            }
+        }
+
+        private void popupWindowShowActionAgrgrBtcr_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            AgregarBitacora newObj;
+
+            newObj = objectSpace.FindObject<AgregarBitacora>(null);
+            if (newObj != null)
+                objectSpace.Delete(newObj);
+
+            newObj = objectSpace.CreateObject<AgregarBitacora>();
+            e.View = Application.CreateDetailView(objectSpace, "AgregarBitacora_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionAgrgrBtcr_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            AgregarBitacora ca = e.PopupWindowViewCurrentObject as AgregarBitacora;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Incidencia obj = View.CurrentObject as Incidencia;
+
+
+                Bitacora btcr = os.CreateObject<Bitacora>();
+
+                btcr.FchFin = ca.Fch;
+                btcr.TimeFin = ca.Tm;
+                btcr.Dscrpcn = ca.Dscrpcn;
+                btcr.Stts = EBITCORATP.Proceso;
+
+                obj.Partidas.Add(btcr);
+                os.CommitChanges();
+            }
+        }
+
+        private void simpleActionCnclr_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            Incidencia obj = View.CurrentObject as Incidencia;
+
+            if (obj != null)
+            {
+                NegocioSrvc.CancelaIncidencia(obj);
+                View.ObjectSpace.CommitChanges();
+            }
+        }
+    }
+}
