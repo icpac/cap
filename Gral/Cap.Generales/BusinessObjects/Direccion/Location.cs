716e00a9d161adb30c686ca90c7157c0e6aac57f
Tlacaelel Icpac <cj.icpac@gmail.com>
2021-03-09 15:13:05 -0600

    Add files via upload

---
+++ Gral/Cap.Generales/BusinessObjects/Direccion/Location.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace Cap.Generales.BusinessObjects.Direccion
+{
+    //[DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("Title")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Location : BaseObject, IMapsMarker
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public Location(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue("PersistentProperty", ref _PersistentProperty, value); }
+        //}
+
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
+        //    this.PersistentProperty = "Paid";
+        //}
+
+
+        private string title;
+        [XafDisplayName("Etiqueta")]
+        public string Title
+        {
+            get { return title; }
+            set { SetPropertyValue("Title", ref title, value); }
+        }
+
+        private double latitude;
+        [XafDisplayName("Latitud")]
+        public double Latitude
+        {
+            get { return latitude; }
+            set { SetPropertyValue("Latitude", ref latitude, value); }
+        }
+
+        private double longitude;
+        [XafDisplayName("Longitud")]
+        public double Longitude
+        {
+            get { return longitude; }
+            set { SetPropertyValue("Longitude", ref longitude, value); }
+        }
+
+        public string IndividualMarkerIcon { get; set; }
+
+        public override string ToString()
+        {
+            string latitudePrefix = Latitude > 0 ? "N" : "S";
+            string longitudePrefix = Longitude > 0 ? "E" : "W";
+            return string.Format("{0}{1:0.###}, {2}{3:0.###}", latitudePrefix, Math.Abs(Latitude), longitudePrefix, Math.Abs(Longitude));
+        }
+    }
+}
